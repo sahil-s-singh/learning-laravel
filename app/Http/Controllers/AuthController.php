313dd4ad6d30b45972128b09a0b40ad1d94ecf66
sahil-s-singh <68877454+sahil-s-singh@users.noreply.github.com>
2022-12-06 01:41:24 -0600

    auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\{
+    StoreUserRequest,
+    LoginUserRequest
+};
+use App\Traits\HttpResponses;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\{
+    Hash,
+    Auth
+};
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    use HttpResponses;
+
+    public function login(LoginUserRequest $request)
+    {
+        $request->validated($request->all());
+
+        if(!Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            return $this->error('', 'Credentials do not match', 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('Token of' . $user->name)->plainTextToken
+        ]);
+        
+    }
+
+    public function register(StoreUserRequest $request)
+    {
+        $request->validated($request->all());
+
+        $user = User:: create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('Token of' . $user->name)->plainTextToken
+        ]);
+    }
+
+    public function logout()
+    {
+        return response()->json('logout method');
+    }
+}
+ 
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3952eea9831c594ece2c81de35b6dc71a8bf983d',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -374,9 +374,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3952eea9831c594ece2c81de35b6dc71a8bf983d',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
